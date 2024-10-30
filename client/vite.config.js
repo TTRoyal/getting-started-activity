3d4e21c223f824bf4de10a10fde31fd6e230c7a6
TT Royal <78960032+TTRoyal@users.noreply.github.com>
2024-10-30 18:56:21 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3004',
+        target: 'https://discordauth.retown.workers.dev/',
         changeOrigin: true,
         secure: false,
         ws: true,
